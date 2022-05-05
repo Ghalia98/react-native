6dde7db27e26d4cfadee819432d30ee319e69811
Ghalia AlHasan <ghaliaalhasan12@gmail.com>
2022-05-05 04:52:59 +0300

    learn to navigate between screens

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>ok </Text>;
+const HomeScreen = ({ navigation }) => {
+  return <View>
+    <Text style={styles.text}>Home Screen</Text>
+    <Button title="Components Demo" onPress={() => navigation.navigate('Components')} />
+    <Button title="List Demo" onPress={() => navigation.navigate('List')} />
+    {/* <TouchableOpacity onPress={() => console.log('Text pressed!')}>
+      <Text>
+        List Demo
+      </Text>
+    </TouchableOpacity> */}
+  </View>
 };
 
 const styles = StyleSheet.create({
